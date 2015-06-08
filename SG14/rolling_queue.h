b3c3d710ad0e715617cac7e5ec43087a419f8075
hatcat <guy@hatcat.com>
2015-06-08 22:11:51 +0100

    First implementation of rolling_queue
    Implementation plus test harness

---
+++ SG14/rolling_queue.h
@@ -0,0 +1,175 @@
+#include <array>
+
+namespace sg14
+{
+	template<typename T, const std::size_t capacity>
+	class rolling_queue
+	{
+	public:
+		typedef rolling_queue<T, capacity> Type;
+		typedef std::array<T, capacity> Container;
+		typedef typename Container::value_type value_type;
+		typedef typename Container::size_type size_type;
+		typedef typename Container::reference reference;
+		typedef typename Container::const_reference const_reference;
+		typedef typename Container::iterator iterator;
+		typedef typename Container::const_iterator const_iterator;
+		typedef typename Container::reverse_iterator reverse_iterator;
+		typedef typename Container::const_reverse_iterator const_reverse_iterator;
+
+		rolling_queue()
+			: c()
+			, count(0)
+			, next_element(std::begin(c))
+			, last_element(next_element)
+		{
+		}
+
+		rolling_queue(const Type& rhs)
+			: c(rhs.c)
+			, count(rhs.count)
+			, next_element(std::begin(c) + (rhs.next_element - std::begin(rhs.c)))
+			, last_element(std::begin(c) + (rhs.last_element - std::begin(rhs.c)))
+		{
+		}
+
+		rolling_queue(Type&& rhs)
+			: c(std::move(rhs.c))
+			, count(std::move(rhs.count))
+			, next_element(std::begin(c) + (rhs.next_element - std::begin(rhs.c)))
+			, last_element(std::begin(c) + (rhs.last_element - std::begin(rhs.c)))
+		{
+		}
+
+		Type& operator=(const Type& rhs)
+		{
+			c = rhs.c;
+			return (*this);
+		}
+
+		Type& operator=(Type&& rhs)
+		{
+			c = std::move(rhs.c);
+			return (*this);
+		}
+
+		bool push(const value_type& from_value)
+		{
+			if (count == capacity)
+			{
+				return false;
+			}
+			*next_element = from_value;
+			increase_back();
+			return true;
+		}
+
+		bool push(value_type&& from_value)
+		{
+			if (count == capacity)
+			{
+				return false;
+			}
+			*next_element = std::move(from_value);
+			increase_back();
+			return true;
+		}
+
+		template<class... FromType>
+		bool emplace(FromType&&... from_value)
+		{
+			if (count == capacity)
+			{
+				return false;
+			}
+			*next_element = T(std::forward<FromType>(from_value)...);
+			increase_back();
+			return true;
+		}
+
+		void pop()
+		{
+			*last_element = T();
+			increase_front();
+		}
+
+		bool empty() const
+		{
+			return (next_element == last_element);
+		}
+
+		size_type size() const
+		{
+			return count;
+		}
+
+		reference front()
+		{
+			return (*last_element);
+		}
+
+		const_reference front() const
+		{
+			return (*last_element);
+		}
+
+		reference back()
+		{
+			auto it = decrease(next_element);
+			return (*it);
+		}
+
+		const_reference back() const
+		{
+			auto it = decrease(next_element);
+			return (*it);
+		}
+
+		void swap(Type& rhs)
+		{
+			auto lhs_next = next_element - c.begin();
+			auto lhs_last = last_element - c.begin();
+			auto rhs_next = rhs.next_element - rhs.c.begin();
+			auto rhs_last = rhs.last_element - rhs.c.begin();
+			std::swap(c, rhs.c);
+			std::swap(count, rhs.count);
+			next_element = c.begin() + rhs_next;
+			last_element = c.begin() + rhs_last;
+			rhs.next_element = rhs.c.begin() + lhs_next;
+			rhs.last_element = rhs.c.begin() + lhs_last;
+		}
+
+	private:
+		Container c;
+		size_t count;
+		iterator next_element;
+		iterator last_element;
+
+		void increase_back()
+		{
+			if (++next_element == c.end())
+			{
+				next_element = c.begin();
+			}
+			++count;
+		}
+
+		iterator decrease(iterator it)
+		{
+			if (it == c.begin())
+			{
+				it = c.end();
+			}
+			return --it;
+		}
+
+		void increase_front()
+		{
+			if (++last_element == c.end())
+			{
+				last_element = c.begin();
+			}
+			--count;
+		}
+	};
+}
---
+++ SG14_test/SG14_test.cpp
@@ -0,0 +1,58 @@
+// SG14_test.cpp : Defines the entry point for the console application.
+//
+
+#include "rolling_queue.h"
+#include <SDKDDKVer.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <cassert>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	sg14::rolling_queue<int, 5> Q;
+
+	Q.push(7);
+	Q.push(3);
+	assert(Q.size() == 2);
+	assert(Q.front() == 7);
+
+	Q.pop();
+	assert(Q.size() == 1);
+
+	Q.push(18);
+	auto Q2 = Q;
+	assert(Q2.front() == 3);
+	assert(Q2.back() == 18);
+
+	auto Q3 = std::move(Q2);
+	assert(Q3.front() == 3);
+	assert(Q3.back() == 18);
+
+	sg14::rolling_queue<int, 5> Q4(Q3);
+	assert(Q4.front() == 3);
+	assert(Q4.back() == 18);
+
+	sg14::rolling_queue<int, 5> Q5(std::move(Q3));
+	assert(Q5.front() == 3);
+	assert(Q5.back() == 18);
+	assert(Q5.size() == 2);
+
+	Q5.pop();
+	Q5.pop();
+	assert(Q5.empty());
+
+	sg14::rolling_queue<int, 5> Q6;
+	Q6.push(6);
+	Q6.push(7);
+	Q6.push(8);
+	Q6.push(9);
+	Q6.emplace(10);
+	Q6.swap(Q5);
+	assert(Q6.empty());
+	assert(Q5.size() == 5);
+	assert(Q5.front() == 6);
+	assert(Q5.back() == 10);
+
+	return 0;
+}
+
