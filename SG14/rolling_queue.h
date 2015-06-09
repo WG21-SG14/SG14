a12e1136164a49b947d706e2434f1eb4d9287366
Jonathan Wakely <cxx@kayari.org>
2015-06-09 10:48:15 +0100

    Rename Container to container_type.
    For consistency with std::stack, std::queue etc.

--- SG14/rolling_queue.h
+++ SG14/rolling_queue.h
@@ -6,15 +6,15 @@ namespace sg14
 	class rolling_queue
 	{
 	public:
-		typedef std::array<T, capacity> Container;
-		typedef typename Container::value_type value_type;
-		typedef typename Container::size_type size_type;
-		typedef typename Container::reference reference;
-		typedef typename Container::const_reference const_reference;
-		typedef typename Container::iterator iterator;
-		typedef typename Container::const_iterator const_iterator;
-		typedef typename Container::reverse_iterator reverse_iterator;
-		typedef typename Container::const_reverse_iterator const_reverse_iterator;
+		typedef std::array<T, capacity> container_type;
+		typedef typename container_type::value_type value_type;
+		typedef typename container_type::size_type size_type;
+		typedef typename container_type::reference reference;
+		typedef typename container_type::const_reference const_reference;
+		typedef typename container_type::iterator iterator;
+		typedef typename container_type::const_iterator const_iterator;
+		typedef typename container_type::reverse_iterator reverse_iterator;
+		typedef typename container_type::const_reverse_iterator const_reverse_iterator;
 
 		rolling_queue() noexcept(std::is_nothrow_default_constructible<T>::value)
 			: c()
@@ -139,7 +139,7 @@ namespace sg14
 		}
 
 	private:
-		Container c;
+		container_type c;
 		size_t count;
 		iterator next_element;
 		iterator last_element;
