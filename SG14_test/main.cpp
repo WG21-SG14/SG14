ccc8fcb0ec2dc6f773a25985a3c1e38cbc0ad92a
John McFarlane <mcfarlane.john@gmail.com>
2015-08-14 08:22:04 -0700

    vs2015 now uses same main function as cmake
    - from `_tmain` to `main`
    - simplifies main.cpp

--- SG14_test/main.cpp
+++ SG14_test/main.cpp
@@ -1,17 +1,12 @@
 #if defined(_MSC_VER)
 #include <SDKDDKVer.h>
-#include <tchar.h>
 #endif
 
 #include <stdio.h>
 
 #include "SG14_test.h"
 
-#if defined(_MSC_VER)
-int _tmain(int argc, _TCHAR* argv[])
-#else
 int main(int, char *[])
-#endif
 {
 	sg14_test::rolling_queue_test();
 	sg14_test::unstable_remove_test();
