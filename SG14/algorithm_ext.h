897d6443517edbf8facae586729b89589f871c60
schmaltzlicker <fourthgeek@gmail.com>
2015-08-14 00:59:49 -0500

    unstable_remove algorithms
    Adds unstable_remove algorithms with performance tests demonstrating
    their benefit.
    Includes an implementation of std::remove_if to ensure fair comparisons
    are being made

--- SG14/algorithm_ext.h
+++ SG14/algorithm_ext.h
@@ -13,8 +13,8 @@ namespace stdext
 		}
 	}
 
-	template<class T, class _Pr>
-	inline T unstable_remove_if(T first, T last, _Pr p)
+	template<class BidirIt, class UnaryPredicate>
+	BidirIt unstable_remove_if(BidirIt first, BidirIt last, UnaryPredicate p)
 	{
 		while (1) {
 			while ((first != last) && p(*first)) {
@@ -26,6 +26,39 @@ namespace stdext
 			}
 			if (first == last) break;
 			*first++ = std::move(*last);
+		}
+		return first;
+	}
+	template<class BidirIt, class Val>
+	BidirIt unstable_remove(BidirIt first, BidirIt last, Val v)
+	{
+		while (1) {
+			while ((first != last) && (*first == v)) {
+				++first;
+			}
+			if (first == last--) break;
+			while ((first != last) && !(*last == v)) {
+				--last;
+			}
+			if (first == last) break;
+			*first++ = std::move(*last);
+		}
+		return first;
+	}
+	//this exists as a point of reference for providing a stable comparison vs unstable_remove_if
+	template<class BidirIt, class UnaryPredicate>
+	BidirIt partition(BidirIt first, BidirIt last, UnaryPredicate p)
+	{
+		while (1) {
+			while ((first != last) && p(*first)) {
+				++first;
+			}
+			if (first == last--) break;
+			while ((first != last) && !p(*last)) {
+				--last;
+			}
+			if (first == last) break;
+			std::iter_swap(first++, last);
 		}
 		return first;
 	}
--- VS2015/SG14/SG14_test/unstable_remove_test.cpp
+++ VS2015/SG14/SG14_test/unstable_remove_test.cpp
@@ -9,8 +9,7 @@
 
 struct foo
 {
-	std::array<uint32_t, 32> info;
-
+	std::array<int,15> info;
 	static foo make()
 	{
 		foo result;
@@ -18,20 +17,29 @@ struct foo
 		return result;
 	}
 };
+template<class T, class U>
+void* operator new(size_t s, std::raw_storage_iterator<T, U> it)
+{
+	return ::operator new(s, it.base());
+}
 
 void sg14_test::unstable_remove_test()
 {
+#if 0
 	std::vector<foo> list1;
-	std::generate_n(std::back_inserter(list1), 1600000, foo::make);
+	std::generate_n(std::back_inserter(list1), 3000000, foo::make);
 	auto list2 = list1;
 
 	auto cmp = [](foo& f) {return f.info[0] & 1; };
 	auto t0 = clock();
-	stdext::unstable_remove_if(list1.begin(), list1.end(), cmp);
+	stdext::partition(list2.begin(), list2.end(), cmp);
 	auto t1 = clock();
-	std::partition(list2.begin(), list2.end(), cmp);
+	stdext::unstable_remove_if(list1.begin(), list1.end(), cmp);
+
 	auto t2 = clock();
 
-	std::cout << "partition: " << t2 - t1 << "\n";
-	std::cout << "unstable: " << t1 - t0 << "\n";
+	std::cout << "partition: " << t1 - t0 << "\n";
+	std::cout << "unstable: " << t2 - t1 << "\n";
+	std::cin.get();
+#endif
 }
