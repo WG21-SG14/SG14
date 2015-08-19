5cc9ea9c1bca4c34e1a86ffa3364fba481d4318c
John McFarlane <john@zoox.com>
2015-08-19 13:11:56 -0700

    display text message after tests have completed

--- SG14_test/main.cpp
+++ SG14_test/main.cpp
@@ -11,6 +11,8 @@ int main(int, char *[])
 	sg14_test::rolling_queue_test();
 	sg14_test::unstable_remove_test();
 	sg14_test::fixed_point_test();
+    
+    puts("tests completed");
 	return 0;
 }
 
