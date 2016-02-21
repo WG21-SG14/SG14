a5190ffd62c2f2f641a41b7526589f029f26db09
Scott Wardle <swardle@gmail.com>
2016-02-21 10:58:39 -0800

    set the optimization setting correctly for GCC

--- Proposals/ComparingVirtualMethods/cpp_entity_example/make_sub.sh
+++ Proposals/ComparingVirtualMethods/cpp_entity_example/make_sub.sh
@@ -2,10 +2,15 @@
 touch CMakeLists.txt
 # export LDFLAGS=-L/usr/local/opt/isl014/lib
 # export CPPFLAGS=-I/usr/local/opt/isl014/include
-mkdir out
-cd out
-cmake -G"Sublime Text 2 - Unix Makefiles"  ..
-# cmake -G"Sublime Text 2 - Unix Makefiles"  ..
-subl --project Main.sublime-project
-cd ..
+# mkdir optdebug
+# pushd optdebug
+# cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G"Sublime Text 2 - Unix Makefiles"  ..
+# subl --project Main.sublime-project
+# popd
 
+mkdir release
+pushd release
+cmake -DCMAKE_BUILD_TYPE=Release -G"Sublime Text 2 - Unix Makefiles"  ..
+subl --project Main.sublime-project
+popd
+# cmake -G"Sublime Text 2 - Unix Makefiles"  ..
