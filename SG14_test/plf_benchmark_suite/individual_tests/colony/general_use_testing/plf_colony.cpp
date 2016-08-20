b6c9bee5488485de4f164ef2fc75710632042d7c
mattreecebentley <mattreecebentley@gmail.com>
2016-08-21 00:16:14 +1200

    Minor corrections

--- SG14_test/plf_benchmark_suite/individual_tests/colony/general_use_testing/plf_colony.cpp
+++ SG14_test/plf_benchmark_suite/individual_tests/colony/general_use_testing/plf_colony.cpp
@@ -6,9 +6,9 @@ int main(int argc, char **argv)
 	output_to_csv_file(argv[0]);
 
 	// Test 1%, 5%, 10% erasure/insertion of container content per minute of gameplay (assuming 60fps) for half an hour
-	benchmark_range_general_use_percentage< plf::colony<small_struct> >(100, 100000, 1.1, 108000, 1, 7, 4, true);
-	benchmark_range_general_use_percentage< plf::colony<small_struct> >(100, 100000, 1.1, 108000, 5, 7, 4, true);
-	benchmark_range_general_use_percentage< plf::colony<small_struct> >(100, 100000, 1.1, 108000, 10, 11, 2, true);
+	benchmark_range_general_use_percentage< plf::colony<small_struct> >(100, 100000, 1.2, 108000, 1, 7, 4, true);
+	benchmark_range_general_use_percentage< plf::colony<small_struct> >(100, 100000, 1.2, 108000, 5, 7, 4, true);
+	benchmark_range_general_use_percentage< plf::colony<small_struct> >(100, 100000, 1.2, 108000, 10, 11, 2, true);
 
 	// Test 1%, 5%, 10% erasure/insertion of container content per frame (assuming 60fps) for half an hour
 	benchmark_range_general_use_percentage2< plf::colony<small_struct> >(100, 100000, 1.2, 108000, .00028, 1, 1, true);
