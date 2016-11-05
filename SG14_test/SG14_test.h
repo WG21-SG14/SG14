e203639721d1e7a2eb1847cb902e6a404fbbbbd8
davidziman <kumquats.a.pair@gmail.com>
2016-11-05 18:33:44 -0500

    Add the dynamic and static ring tests. These tests should fail whenever executed because the contents of them have been conditionally compiled out as the functions they call are not defined.

--- SG14_test/SG14_test.h
+++ SG14_test/SG14_test.h
@@ -3,9 +3,11 @@
 
 namespace sg14_test
 {
-        void inplace_function_test();
-        void transcode_test();
-        void ring_test();
+    void inplace_function_test();
+    void transcode_test();
+    void ring_test();
+    void static_ring_test();
+    void dynamic_ring_test();
 	void thread_communication_test();
 	void unstable_remove_test();
 	void uninitialized();
