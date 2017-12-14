2747db8b94900f15c457b5849bb803f0b829e264
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-12-14 08:45:27 -0800

    inplace_function.h: re-indent to 4-space tabs

--- SG14/inplace_function.h
+++ SG14/inplace_function.h
@@ -24,86 +24,82 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-// TODO: infer size of capture via decltype?
-
 #pragma once
 
 #include <type_traits>
 #include <utility>
 #include <functional>
 
-namespace stdext
-{
+namespace stdext {
 
-namespace detail
-{
+namespace detail {
 
 static constexpr size_t InplaceFunctionDefaultCapacity = 32;
 
 using default_storage_t = std::aligned_storage_t<
-  InplaceFunctionDefaultCapacity
+    InplaceFunctionDefaultCapacity
 >;
 
 static constexpr size_t InplaceFunctionDefaultAlignment = std::alignment_of<
-  default_storage_t
+    default_storage_t
 >::value;
 
 
 template<typename T> struct wrapper
 {
-  using type = T;
+    using type = T;
 };
 
 template<typename R, typename... Args> struct vtable
 {
-  using storage_ptr_t = void*;
+    using storage_ptr_t = void*;
 
-  using invoke_ptr_t = R(*)(storage_ptr_t, Args&&...);
-  using process_ptr_t = void(*)(storage_ptr_t, storage_ptr_t);
-	using destructor_ptr_t = void(*)(storage_ptr_t);
+    using invoke_ptr_t = R(*)(storage_ptr_t, Args&&...);
+    using process_ptr_t = void(*)(storage_ptr_t, storage_ptr_t);
+    using destructor_ptr_t = void(*)(storage_ptr_t);
 
-  const invoke_ptr_t invoke_ptr;
-  const process_ptr_t copy_ptr;
-  const process_ptr_t move_ptr;
-  const destructor_ptr_t destructor_ptr;
+    const invoke_ptr_t invoke_ptr;
+    const process_ptr_t copy_ptr;
+    const process_ptr_t move_ptr;
+    const destructor_ptr_t destructor_ptr;
 
-  explicit constexpr vtable() noexcept :
-    invoke_ptr{ [](storage_ptr_t, Args&&...) -> R
-      { throw std::bad_function_call(); }
-    },
-    copy_ptr{ [](storage_ptr_t, storage_ptr_t) noexcept -> void {} },
-    move_ptr{ [](storage_ptr_t, storage_ptr_t) noexcept -> void {} },
-    destructor_ptr{ [](storage_ptr_t) noexcept -> void {} }
-  {}
+    explicit constexpr vtable() noexcept :
+        invoke_ptr{ [](storage_ptr_t, Args&&...) -> R
+            { throw std::bad_function_call(); }
+        },
+        copy_ptr{ [](storage_ptr_t, storage_ptr_t) noexcept -> void {} },
+        move_ptr{ [](storage_ptr_t, storage_ptr_t) noexcept -> void {} },
+        destructor_ptr{ [](storage_ptr_t) noexcept -> void {} }
+    {}
 
-  template<typename C> explicit constexpr vtable(wrapper<C>) noexcept :
-    invoke_ptr{ [](storage_ptr_t storage_ptr, Args&&... args)
-      noexcept(noexcept(std::declval<C>()(args...))) -> R
-      { return (*static_cast<C*>(storage_ptr))(
-        std::forward<Args>(args)...
-      ); }
-    },
-    copy_ptr{ [](storage_ptr_t dst_ptr, storage_ptr_t src_ptr)
-      noexcept(std::is_nothrow_copy_constructible<C>::value) -> void
-      { new (dst_ptr) C{ (*static_cast<C*>(src_ptr)) }; }
-    },
-    move_ptr{ [](storage_ptr_t dst_ptr, storage_ptr_t src_ptr)
-      noexcept(std::is_nothrow_move_constructible<C>::value) -> void
-      { new (dst_ptr) C{ std::move(*static_cast<C*>(src_ptr)) }; }
-    },
-    destructor_ptr{ [](storage_ptr_t storage_ptr)
-      noexcept -> void
-      { static_cast<C*>(storage_ptr)->~C(); }
-    }
-  {}
+    template<typename C> explicit constexpr vtable(wrapper<C>) noexcept :
+        invoke_ptr{ [](storage_ptr_t storage_ptr, Args&&... args)
+            noexcept(noexcept(std::declval<C>()(args...))) -> R
+            { return (*static_cast<C*>(storage_ptr))(
+                std::forward<Args>(args)...
+            ); }
+        },
+        copy_ptr{ [](storage_ptr_t dst_ptr, storage_ptr_t src_ptr)
+            noexcept(std::is_nothrow_copy_constructible<C>::value) -> void
+            { new (dst_ptr) C{ (*static_cast<C*>(src_ptr)) }; }
+        },
+        move_ptr{ [](storage_ptr_t dst_ptr, storage_ptr_t src_ptr)
+            noexcept(std::is_nothrow_move_constructible<C>::value) -> void
+            { new (dst_ptr) C{ std::move(*static_cast<C*>(src_ptr)) }; }
+        },
+        destructor_ptr{ [](storage_ptr_t storage_ptr)
+            noexcept -> void
+            { static_cast<C*>(storage_ptr)->~C(); }
+        }
+    {}
 
-  vtable(const vtable&) = delete;
-  vtable(vtable&&) = delete;
+    vtable(const vtable&) = delete;
+    vtable(vtable&&) = delete;
 
-  vtable& operator= (const vtable&) = delete;
-  vtable& operator= (vtable&&) = delete;
+    vtable& operator= (const vtable&) = delete;
+    vtable& operator= (vtable&&) = delete;
 
-  ~vtable() = default;
+    ~vtable() = default;
 };
 
 template<typename R, typename... Args>
@@ -112,223 +108,223 @@ inline constexpr vtable<R, Args...> empty_vtable{};
 template<size_t DstCap, size_t DstAlign, size_t SrcCap, size_t SrcAlign>
 struct is_valid_inplace_dst : std::true_type
 {
-  static_assert(DstCap >= SrcCap,
-    "Can't squeeze larger inplace_function into a smaller one"
-  );
+    static_assert(DstCap >= SrcCap,
+        "Can't squeeze larger inplace_function into a smaller one"
+    );
 
-  static_assert(DstAlign % SrcAlign == 0,
-    "Incompatible inplace_function alignments"
-  );
+    static_assert(DstAlign % SrcAlign == 0,
+        "Incompatible inplace_function alignments"
+    );
 };
 
 } // namespace detail
 
 template<
-  typename,
-  size_t Capacity = detail::InplaceFunctionDefaultCapacity,
-  size_t Alignment = detail::InplaceFunctionDefaultAlignment
+    typename,
+    size_t Capacity = detail::InplaceFunctionDefaultCapacity,
+    size_t Alignment = detail::InplaceFunctionDefaultAlignment
 >
 class inplace_function; // unspecified
 
 template<
-  typename R,
-  typename... Args,
-  size_t Capacity,
-  size_t Alignment
+    typename R,
+    typename... Args,
+    size_t Capacity,
+    size_t Alignment
 >
 class inplace_function<R(Args...), Capacity, Alignment>
 {
 public:
-  using capacity = std::integral_constant<size_t, Capacity>;
-  using alignment = std::integral_constant<size_t, Alignment>;
+    using capacity = std::integral_constant<size_t, Capacity>;
+    using alignment = std::integral_constant<size_t, Alignment>;
 
-  using storage_t = std::aligned_storage_t<Capacity, Alignment>;
-  using vtable_t = detail::vtable<R, Args...>;
-  using vtable_ptr_t = const vtable_t*;
+    using storage_t = std::aligned_storage_t<Capacity, Alignment>;
+    using vtable_t = detail::vtable<R, Args...>;
+    using vtable_ptr_t = const vtable_t*;
 
-  template <typename, size_t, size_t>	friend class inplace_function;
+    template <typename, size_t, size_t>	friend class inplace_function;
 
-  inplace_function() noexcept :
-    vtable_ptr_{std::addressof(detail::empty_vtable<R, Args...>)}
-  {}
+    inplace_function() noexcept :
+        vtable_ptr_{std::addressof(detail::empty_vtable<R, Args...>)}
+    {}
 
-  template<
-    typename T,
-    typename C = std::decay_t<T>,
-    typename = std::enable_if_t<
-      !(std::is_same<C, inplace_function>::value
-      || std::is_convertible<C, inplace_function>::value)
+    template<
+        typename T,
+        typename C = std::decay_t<T>,
+        typename = std::enable_if_t<
+            !(std::is_same<C, inplace_function>::value
+            || std::is_convertible<C, inplace_function>::value)
+        >
     >
-  >
-  inplace_function(T&& closure)
-  {
-    // C++17
-    //static_assert(std::is_invocable_r<R, C, Args...>::value,
-    //  "Function closure has to be invocable"
-    //);
-
-    static_assert(std::is_copy_constructible<C>::value,
-      "Constructing function with move only type is invalid"
-    );
-
-    static_assert(sizeof(C) <= Capacity, "Inplace function closure too large");
-
-    static_assert(
-      Alignment % std::alignment_of<C>::value == 0,
-      "Incompatible function closure alignment"
-    );
-
-    static const vtable_t vt{detail::wrapper<C>{}};
-    vtable_ptr_ = std::addressof(vt);
-
-    new (std::addressof(storage_)) C{std::forward<T>(closure)};
-  }
-
-  inplace_function(std::nullptr_t) noexcept :
-    vtable_ptr_{std::addressof(detail::empty_vtable<R, Args...>)}
-  {}
-
-  inplace_function(const inplace_function& other) :
-    vtable_ptr_{other.vtable_ptr_}
-  {
-    vtable_ptr_->copy_ptr(
-      std::addressof(storage_),
-      std::addressof(other.storage_)
-    );
-  }
-
-  inplace_function(inplace_function&& other) :
-    vtable_ptr_{other.vtable_ptr_}
-  {
-    vtable_ptr_->move_ptr(
-      std::addressof(storage_),
-      std::addressof(other.storage_)
-    );
-  }
-
-  inplace_function& operator= (std::nullptr_t) noexcept
-  {
-    vtable_ptr_->destructor_ptr(std::addressof(storage_));
-    vtable_ptr_ = std::addressof(detail::empty_vtable<R, Args...>);
-    return *this;
-  }
-
-  inplace_function& operator= (const inplace_function& other)
-  {
-    if(this != std::addressof(other))
+    inplace_function(T&& closure)
     {
-      vtable_ptr_->destructor_ptr(std::addressof(storage_));
+        // C++17
+        //static_assert(std::is_invocable_r<R, C, Args...>::value,
+        //    "Function closure has to be invocable"
+        //);
 
-      vtable_ptr_ = other.vtable_ptr_;
-      vtable_ptr_->copy_ptr(
-        std::addressof(storage_),
-        std::addressof(other.storage_)
-      );
+        static_assert(std::is_copy_constructible<C>::value,
+            "Constructing function with move only type is invalid"
+        );
+
+        static_assert(sizeof(C) <= Capacity, "Inplace function closure too large");
+
+        static_assert(
+            Alignment % std::alignment_of<C>::value == 0,
+            "Incompatible function closure alignment"
+        );
+
+        static const vtable_t vt{detail::wrapper<C>{}};
+        vtable_ptr_ = std::addressof(vt);
+
+        new (std::addressof(storage_)) C{std::forward<T>(closure)};
     }
-    return *this;
-  }
 
-  inplace_function& operator= (inplace_function&& other)
-  {
-    if(this != std::addressof(other))
+    inplace_function(std::nullptr_t) noexcept :
+        vtable_ptr_{std::addressof(detail::empty_vtable<R, Args...>)}
+    {}
+
+    inplace_function(const inplace_function& other) :
+        vtable_ptr_{other.vtable_ptr_}
     {
-      vtable_ptr_->destructor_ptr(std::addressof(storage_));
-
-      vtable_ptr_ = other.vtable_ptr_;
-      vtable_ptr_->move_ptr(
-        std::addressof(storage_),
-        std::addressof(other.storage_)
-      );
+        vtable_ptr_->copy_ptr(
+            std::addressof(storage_),
+            std::addressof(other.storage_)
+        );
     }
-    return *this;
-  }
 
-  ~inplace_function()
-  {
-    vtable_ptr_->destructor_ptr(std::addressof(storage_));
-  }
+    inplace_function(inplace_function&& other) :
+        vtable_ptr_{other.vtable_ptr_}
+    {
+        vtable_ptr_->move_ptr(
+            std::addressof(storage_),
+            std::addressof(other.storage_)
+        );
+    }
 
-  R operator() (Args... args) const
-  {
-    return vtable_ptr_->invoke_ptr(
-      std::addressof(storage_),
-      std::forward<Args>(args)...
-    );
-  }
+    inplace_function& operator= (std::nullptr_t) noexcept
+    {
+        vtable_ptr_->destructor_ptr(std::addressof(storage_));
+        vtable_ptr_ = std::addressof(detail::empty_vtable<R, Args...>);
+        return *this;
+    }
 
-  constexpr bool operator== (std::nullptr_t) const noexcept
-  {
-    return !operator bool();
-  }
+    inplace_function& operator= (const inplace_function& other)
+    {
+        if(this != std::addressof(other))
+        {
+            vtable_ptr_->destructor_ptr(std::addressof(storage_));
 
-  constexpr bool operator!= (std::nullptr_t) const noexcept
-  {
-    return operator bool();
-  }
+            vtable_ptr_ = other.vtable_ptr_;
+            vtable_ptr_->copy_ptr(
+                std::addressof(storage_),
+                std::addressof(other.storage_)
+            );
+        }
+        return *this;
+    }
 
-  explicit constexpr operator bool() const noexcept
-  {
-    return vtable_ptr_ != std::addressof(detail::empty_vtable<R, Args...>);
-  }
+    inplace_function& operator= (inplace_function&& other)
+    {
+        if(this != std::addressof(other))
+        {
+            vtable_ptr_->destructor_ptr(std::addressof(storage_));
 
-  template<size_t Cap, size_t Align>
-  operator inplace_function<R(Args...), Cap, Align>() const&
-  {
-    static_assert(detail::is_valid_inplace_dst<
-      Cap, Align, Capacity, Alignment
-    >::value);
+            vtable_ptr_ = other.vtable_ptr_;
+            vtable_ptr_->move_ptr(
+                std::addressof(storage_),
+                std::addressof(other.storage_)
+            );
+        }
+        return *this;
+    }
 
-    return {vtable_ptr_, vtable_ptr_->copy_ptr, std::addressof(storage_)};
-  }
+    ~inplace_function()
+    {
+        vtable_ptr_->destructor_ptr(std::addressof(storage_));
+    }
 
-  template<size_t Cap, size_t Align>
-  operator inplace_function<R(Args...), Cap, Align>() &&
-  {
-    static_assert(detail::is_valid_inplace_dst<
-      Cap, Align, Capacity, Alignment
-    >::value);
+    R operator() (Args... args) const
+    {
+        return vtable_ptr_->invoke_ptr(
+            std::addressof(storage_),
+            std::forward<Args>(args)...
+        );
+    }
 
-    return {vtable_ptr_, vtable_ptr_->move_ptr, std::addressof(storage_)};
-  }
+    constexpr bool operator== (std::nullptr_t) const noexcept
+    {
+        return !operator bool();
+    }
 
-  void swap(inplace_function& other)
-  {
-    if (this == std::addressof(other)) return;
+    constexpr bool operator!= (std::nullptr_t) const noexcept
+    {
+        return operator bool();
+    }
 
-    storage_t tmp;
-    vtable_ptr_->move_ptr(
-      std::addressof(tmp),
-      std::addressof(storage_)
-    );
-    vtable_ptr_->destructor_ptr(std::addressof(storage_));
+    explicit constexpr operator bool() const noexcept
+    {
+        return vtable_ptr_ != std::addressof(detail::empty_vtable<R, Args...>);
+    }
 
-    other.vtable_ptr_->move_ptr(
-      std::addressof(storage_),
-      std::addressof(other.storage_)
-    );
-    other.vtable_ptr_->destructor_ptr(std::addressof(other.storage_));
+    template<size_t Cap, size_t Align>
+    operator inplace_function<R(Args...), Cap, Align>() const&
+    {
+        static_assert(detail::is_valid_inplace_dst<
+            Cap, Align, Capacity, Alignment
+        >::value);
 
-    vtable_ptr_->move_ptr(
-      std::addressof(other.storage_),
-      std::addressof(tmp)
-    );
-    vtable_ptr_->destructor_ptr(std::addressof(tmp));
+        return {vtable_ptr_, vtable_ptr_->copy_ptr, std::addressof(storage_)};
+    }
 
-    std::swap(vtable_ptr_, other.vtable_ptr_);
-  }
+    template<size_t Cap, size_t Align>
+    operator inplace_function<R(Args...), Cap, Align>() &&
+    {
+        static_assert(detail::is_valid_inplace_dst<
+            Cap, Align, Capacity, Alignment
+        >::value);
+
+        return {vtable_ptr_, vtable_ptr_->move_ptr, std::addressof(storage_)};
+    }
+
+    void swap(inplace_function& other)
+    {
+        if (this == std::addressof(other)) return;
+
+        storage_t tmp;
+        vtable_ptr_->move_ptr(
+            std::addressof(tmp),
+            std::addressof(storage_)
+        );
+        vtable_ptr_->destructor_ptr(std::addressof(storage_));
+
+        other.vtable_ptr_->move_ptr(
+            std::addressof(storage_),
+            std::addressof(other.storage_)
+        );
+        other.vtable_ptr_->destructor_ptr(std::addressof(other.storage_));
+
+        vtable_ptr_->move_ptr(
+            std::addressof(other.storage_),
+            std::addressof(tmp)
+        );
+        vtable_ptr_->destructor_ptr(std::addressof(tmp));
+
+        std::swap(vtable_ptr_, other.vtable_ptr_);
+    }
 
 private:
-  vtable_ptr_t vtable_ptr_;
-  mutable storage_t storage_;
+    vtable_ptr_t vtable_ptr_;
+    mutable storage_t storage_;
 
-  inplace_function(
-    vtable_ptr_t vtable_ptr,
-    typename vtable_t::process_ptr_t process_ptr,
-    typename vtable_t::storage_ptr_t storage_ptr
-  ) : vtable_ptr_{vtable_ptr}
-  {
-    process_ptr(std::addressof(storage_), storage_ptr);
-  }
+    inplace_function(
+        vtable_ptr_t vtable_ptr,
+        typename vtable_t::process_ptr_t process_ptr,
+        typename vtable_t::storage_ptr_t storage_ptr
+    ) : vtable_ptr_{vtable_ptr}
+    {
+        process_ptr(std::addressof(storage_), storage_ptr);
+    }
 };
 
 } // namespace stdext
