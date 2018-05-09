4f2308bebed4cf94f02f3e96c5eb7eb84a7e9846
mattreecebentley <mattreecebentley@gmail.com>
2018-05-09 13:58:27 +1200

    colony v4.57a
    Reversion of explicit declaration for reverse_iterator constructor from iterator

--- SG14/plf_colony.h
+++ SG14/plf_colony.h
@@ -909,7 +909,7 @@ public:
 
 
 
-		explicit colony_reverse_iterator (const typename colony::iterator &source) PLF_COLONY_NOEXCEPT:
+		colony_reverse_iterator (const typename colony::iterator &source) PLF_COLONY_NOEXCEPT:
 			it(source)
 		{}
 
