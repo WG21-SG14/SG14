WG21-SG14/SG14

commit 6ce9c8bc4e499b97950de9fc0373783b1299313c

Correct static casts for conversions, avoid conversion warnings in msvc2017 warning level 3

SG14/slot_map.h
@@ -219,7 +219,7 @@ public:
         slot_map_detail::reserve_if_possible(slots_, n);
         while (slots_.size() < n) {
             auto idx = next_available_slot_index_;
-            next_available_slot_index_ = slots_.size();
+            next_available_slot_index_ = static_cast<stdext::slot_map<int, std::pair<unsigned int, unsigned int>, std::vector>::key_size_type>(slots_.size());
             slots_.emplace_back(key_type{idx, key_generation_type{}});
         }
     }

commit ef35e7d9f5808b16089bf3afa1b8cdbd75266df3

Fix a bug introduced in 6ce9c8bc4e499b97950de9fc0373783b1299313c.

SG14/slot_map.h
@@ -219,7 +219,7 @@ public:
         slot_map_detail::reserve_if_possible(slots_, n);
         while (slots_.size() < n) {
             auto idx = next_available_slot_index_;
-            next_available_slot_index_ = static_cast<stdext::slot_map<int, std::pair<unsigned int, unsigned int>, std::vector>::key_size_type>(slots_.size());
+            next_available_slot_index_ = static_cast<key_size_type>(slots_.size());
             slots_.emplace_back(key_type{idx, key_generation_type{}});
         }
     }