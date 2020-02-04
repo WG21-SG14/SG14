WG21-SG14/SG14

commit 836e4d202249a86ba5ff56139c35e0afa60e7a1c

colony v5.2 Alterations to try and avoid warnings in MSVC2019 (can't avoid them all)

SG14/slot_map.h
@@ -104,7 +104,7 @@ public:
 
     constexpr slot_map() = default;
     constexpr slot_map(const slot_map&) = default;
-    constexpr slot_map(slot_map&&) = default;
+    constexpr slot_map(slot_map&&) noexcept = default;
     constexpr slot_map& operator=(const slot_map&) = default;
     constexpr slot_map& operator=(slot_map&&) = default;
     ~slot_map() = default;

SG14_test/slot_map_test.cpp
@@ -51,14 +51,14 @@ struct Vector {
     Vector() = default;
     template<class T_ = T, class = std::enable_if_t<std::is_copy_constructible<T_>::value>>
     Vector(const Vector& rhs) { *this = rhs; }
-    Vector(Vector&& rhs) { *this = std::move(rhs); }
+    Vector(Vector&& rhs) noexcept { *this = std::move(rhs); }
     template<class T_ = T, class = std::enable_if_t<std::is_copy_constructible<T_>::value>>
     void operator=(const Vector& rhs) {
         size_ = rhs.size_;
         data_ = std::make_unique<T[]>(size_);
         std::copy(rhs.begin(), rhs.end(), data_.get());
     }
-    void operator=(Vector&& rhs) {
+    void operator=(Vector&& rhs) noexcept {
         size_ = rhs.size_;
         data_ = std::move(rhs.data_);
     }
@@ -452,7 +452,7 @@ static void GenerationsDontSkipTest()
     using T = typename SM::mapped_type;
     SM sm;
     auto k1 = sm.emplace(Monad<T>::from_value(1));
-    int original_cap = sm.slot_count();
+    int original_cap = static_cast<int>(sm.slot_count());
     for (int i=1; i < original_cap; ++i) {
         sm.emplace(Monad<T>::from_value(i));
     }
@@ -480,7 +480,7 @@ static void IndexesAreUsedEvenlyTest()
     SM sm;
     auto k1 = sm.emplace(Monad<T>::from_value(1));
     auto k2 = sm.emplace(Monad<T>::from_value(2));
-    int original_cap = sm.slot_count();
+    int original_cap = static_cast<int>(sm.slot_count());
     for (int i=2; i < original_cap; ++i) {
         sm.emplace(Monad<T>::from_value(i));
     }

SG14/plf_colony.h
@@ -244,7 +244,7 @@ public:
 	typedef element_skipfield_type																		skipfield_type;
 
 	#ifdef PLF_COLONY_ALIGNMENT_SUPPORT
-		typedef typename std::aligned_storage<sizeof(element_type), (alignof(element_type) > (sizeof(element_skipfield_type) * 2)) ? alignof(element_type) : (sizeof(element_skipfield_type) * 2)>::type	aligned_element_type;
+		typedef typename std::aligned_storage<sizeof(element_type), (sizeof(element_type) > (sizeof(element_skipfield_type) * 2)) ? alignof(element_type) : (sizeof(element_skipfield_type) * 2)>::type	aligned_element_type;
 	#else
 		typedef element_type																				aligned_element_type;
 	#endif
@@ -322,7 +322,7 @@ private:
 		const skipfield_pointer_type		skipfield; 					// Skipfield storage. The element and skipfield arrays are allocated contiguously, hence the skipfield pointer also functions as a 'one-past-end' pointer for the elements array. There will always be one additional skipfield node allocated compared to the number of elements. This is to ensure a faster ++ iterator operation (fewer checks are required when this is present). The extra node is unused and always zero, but checked, and not having it will result in out-of-bounds memory errors.
 		group_pointer_type					previous_group; 			// previous group in the intrusive list of all groups. NULL if no preceding group
 		skipfield_type						free_list_head; 			// The index of the last erased element in the group. The last erased element will, in turn, contain the number of the index of the next erased element, and so on. If this is == maximum skipfield_type value then free_list is empty ie. no erasures have occurred in the group (or if they have, the erased locations have then been reused via insert()).
-		const skipfield_type				capacity; 						// The element capacity of this particular group
+		const skipfield_type				capacity; 					// The element capacity of this particular group
 		skipfield_type						number_of_elements; 		// indicates total number of active elements in group - changes with insert and erase commands - used to check for empty group in erase function, as an indication to remove the group
 		group_pointer_type					erasures_list_next_group; 	// The next group in the intrusive singly-linked list of groups with erasures ie. with active erased-element free lists
 		size_type							group_number; 				// Used for comparison (> < >= <=) iterator operators (used by distance function and user)
@@ -348,7 +348,7 @@ private:
 		#else
 			// This is a hack around the fact that element_allocator_type::construct only supports copy construction in C++03 and copy elision does not occur on the vast majority of compilers in this circumstance. And to avoid running out of memory (and losing performance) from allocating the same block twice, we're allocating in this constructor and moving data in the copy constructor.
 			group(const skipfield_type elements_per_group, group_pointer_type const previous = NULL):
-				last_endpoint(reinterpret_cast<aligned_pointer_type>(PLF_COLONY_ALLOCATE_INITIALIZATION(uchar_allocator_type, ((elements_per_group * (sizeof(aligned_element_type))) + ((elements_per_group + 1) * sizeof(skipfield_type))), (previous == NULL) ? 0 : previous->elements))),
+				last_endpoint(reinterpret_cast<aligned_pointer_type>(PLF_COLONY_ALLOCATE_INITIALIZATION(uchar_allocator_type, ((elements_per_group * (sizeof(aligned_element_type))) + (static_cast<size_type>(elements_per_group + 1) * sizeof(skipfield_type))), (previous == NULL) ? 0 : previous->elements))),
 				elements(NULL),
 				skipfield(reinterpret_cast<skipfield_pointer_type>(last_endpoint + elements_per_group)),
 				previous_group(previous),
@@ -965,7 +965,7 @@ private:
 
 
 	iterator				end_iterator, begin_iterator;
-	group_pointer_type	groups_with_erasures_list_head; // Head of a singly-linked intrusive list of groups which have erased-element memory locations available for reuse
+	group_pointer_type		groups_with_erasures_list_head; // Head of a singly-linked intrusive list of groups which have erased-element memory locations available for reuse
 	size_type				total_number_of_elements, total_capacity;
 
 	struct ebco_pair2 : pointer_allocator_type // Packaging the element pointer allocator with a lesser-used member variable, for empty-base-class optimisation
@@ -1893,7 +1893,7 @@ private:
 					}
 					else
 					{
-						alignas (sizeof(aligned_element_type)) element_type aligned_copy = element; // to avoid potentially violating memory boundaries in line below, create an initial copy object of same (but aligned) type
+						alignas (alignof(aligned_element_type)) element_type aligned_copy = element; // to avoid potentially violating memory boundaries in line below, create an initial copy object of same (but aligned) type
 						std::fill_n(end_iterator.element_pointer, number_of_elements, *(reinterpret_cast<aligned_pointer_type>(&aligned_copy)));
 					}
 				#else // type is not aligned to anything so is safe to use fill_n anyway:
@@ -1942,7 +1942,7 @@ private:
 					}
 					else
 					{
-						alignas (sizeof(aligned_element_type)) element_type aligned_copy = element; // to avoid potentially violating memory boundaries in line below, create an initial copy object of same (but aligned) type
+						alignas (alignof(aligned_element_type)) element_type aligned_copy = element; // to avoid potentially violating memory boundaries in line below, create an initial copy object of same (but aligned) type
 						std::fill_n(location, number_of_elements, *(reinterpret_cast<aligned_pointer_type>(&aligned_copy)));
 					}
 				#else // type is not aligned to anything so is safe to use fill_n anyway:
@@ -1965,7 +1965,7 @@ private:
 				{
 					// Reconstruct existing skipblock and free-list indexes to reflect partially-reused skipblock:
 					const skipfield_type elements_constructed_before_exception = static_cast<skipfield_type>((current_location - 1) - location);
-					groups_with_erasures_list_head->number_of_elements += elements_constructed_before_exception;
+					groups_with_erasures_list_head->number_of_elements = static_cast<skipfield_type>(groups_with_erasures_list_head->number_of_elements + elements_constructed_before_exception);
 					total_number_of_elements += elements_constructed_before_exception;
 
 					std::memset(skipfield_pointer, 0, elements_constructed_before_exception * sizeof(skipfield_type));
@@ -1973,7 +1973,7 @@ private:
 					*(reinterpret_cast<skipfield_pointer_type>(location + elements_constructed_before_exception)) = prev_free_list_node;
 					*(reinterpret_cast<skipfield_pointer_type>(location + elements_constructed_before_exception) + 1) = std::numeric_limits<skipfield_type>::max();
 
-					const skipfield_type new_skipblock_head_index = static_cast<skipfield_type>(location - groups_with_erasures_list_head->elements) + elements_constructed_before_exception;
+					const skipfield_type new_skipblock_head_index = static_cast<skipfield_type>(static_cast<skipfield_type>(location - groups_with_erasures_list_head->elements) + elements_constructed_before_exception);
 					groups_with_erasures_list_head->free_list_head = new_skipblock_head_index;
 
 					if (prev_free_list_node != std::numeric_limits<skipfield_type>::max())
@@ -2310,8 +2310,9 @@ public:
 					update_value = following_value;
 					break;
 				}
-				case 3: // both preceding and following consecutive erased elements
+				case 3: // both preceding and following consecutive erased elements - erased element is between two skipblocks
 				{
+               *(it.skipfield_pointer) = 1;  // modification to allow skipfield to be used for SIMD-gather masking
 					const skipfield_type preceding_value = *(it.skipfield_pointer - 1);
 					const skipfield_type following_value = static_cast<skipfield_type>(*(it.skipfield_pointer + 1) + 1);
 
@@ -2440,7 +2441,7 @@ public:
 
 		iterator current = iterator1;
 
-		if (current.group_pointer != iterator2.group_pointer)
+		if (current.group_pointer != iterator2.group_pointer) // ie. if start and end iterators are in separate groups
 		{
 			if (current.element_pointer != current.group_pointer->elements + *(current.group_pointer->skipfield)) // if iterator1 is not the first non-erased element in it's group - most common case
 			{
@@ -2522,10 +2523,13 @@ public:
 				const skipfield_type previous_node_value = *(iterator1.skipfield_pointer - 1);
 				const skipfield_type distance_to_end = static_cast<skipfield_type>(end - iterator1.element_pointer);
 
+				std::memset(&*(iterator1.skipfield_pointer), 1, sizeof(skipfield_type) * (static_cast<size_type>(distance_to_end) - 1)); // modification to allow skipfield to be used for SIMD-gather masking
+
+
 				if (previous_node_value == 0) // no previous skipblock
 				{
-					*iterator1.skipfield_pointer = distance_to_end;
-					*(iterator1.skipfield_pointer + distance_to_end - 1) = distance_to_end;
+					*iterator1.skipfield_pointer = distance_to_end; // set start node value
+					*(iterator1.skipfield_pointer + distance_to_end - 1) = distance_to_end; // set end node value
 
 					const skipfield_type index = static_cast<skipfield_type>(iterator1.element_pointer - iterator1.group_pointer->elements);
 
@@ -2692,6 +2696,9 @@ public:
 
 			const skipfield_type distance_to_iterator2 = static_cast<skipfield_type>(iterator2.element_pointer - current_saved.element_pointer);
 			const skipfield_type index = static_cast<skipfield_type>(current_saved.element_pointer - iterator2.group_pointer->elements);
+
+			std::memset(&*(current_saved.skipfield_pointer + 1), 1, sizeof(skipfield_type) * (static_cast<size_type>(distance_to_iterator2) - 1));  // modification to allow skipfield to be used for SIMD-gather masking
+
 
 			if (index == 0 || *(current_saved.skipfield_pointer - 1) == 0) // element is either at start of group or previous skipfield node is 0
 			{
@@ -3991,6 +3998,53 @@ public:
 		end_iterator = source.end_iterator;
 		total_number_of_elements += source.total_number_of_elements;
 		source.blank();
+	}
+
+
+
+	struct raw_memory_block_pointers : private uchar_allocator_type
+	{
+		aligned_pointer_type *element_memory_block_pointers;		// array of pointers to element memory blocks (allow for scatter back to memory blocks)
+		skipfield_pointer_type *skipfield_memory_block_pointers;	// array of pointers to skipfield memory blocks
+		skipfield_type *block_sizes;								// array of the number of elements in each memory block
+		size_type number_of_blocks;									// size of each array
+
+		raw_memory_block_pointers(const size_type size) :
+			element_memory_block_pointers(reinterpret_cast<aligned_pointer_type *>(PLF_COLONY_ALLOCATE_INITIALIZATION(uchar_allocator_type, size * sizeof(aligned_pointer_type), NULL))),
+			skipfield_memory_block_pointers(reinterpret_cast<skipfield_pointer_type *>(PLF_COLONY_ALLOCATE_INITIALIZATION(uchar_allocator_type, size * sizeof(skipfield_pointer_type), NULL))),
+			block_sizes(reinterpret_cast<skipfield_type *>(PLF_COLONY_ALLOCATE_INITIALIZATION(uchar_allocator_type, size * sizeof(skipfield_type *), NULL))),
+			number_of_blocks(size)
+		{}
+
+		~raw_memory_block_pointers()
+		{
+			PLF_COLONY_DEALLOCATE(uchar_allocator_type, (*this), reinterpret_cast<uchar_pointer_type>(element_memory_block_pointers), number_of_blocks * sizeof(pointer));
+			PLF_COLONY_DEALLOCATE(uchar_allocator_type, (*this), reinterpret_cast<uchar_pointer_type>(skipfield_memory_block_pointers), number_of_blocks * sizeof(skipfield_pointer_type));
+			PLF_COLONY_DEALLOCATE(uchar_allocator_type, (*this), reinterpret_cast<uchar_pointer_type>(block_sizes), number_of_blocks * sizeof(size_type));
+		}
+	};
+
+
+
+	raw_memory_block_pointers * get_raw_memory_block_pointers()
+	{
+		raw_memory_block_pointers *data = new raw_memory_block_pointers(end_iterator.group_pointer->group_number + 1);
+		size_type group_number = 0;
+		
+		for (group_pointer_type current_group = begin_iterator.group_pointer; current_group != end_iterator.group_pointer; current_group = current_group->next_group)
+		{
+			data->element_memory_block_pointers[group_number] = current_group->elements;
+			data->skipfield_memory_block_pointers[group_number] = current_group->skipfield;
+			data->block_sizes[group_number] = current_group->capacity;
+			++group_number;
+		}
+		
+		// Special case for end group:
+		data->element_memory_block_pointers[group_number] = end_iterator.group_pointer->elements;
+		data->skipfield_memory_block_pointers[group_number] = end_iterator.group_pointer->skipfield;
+		data->block_sizes[group_number] = static_cast<skipfield_type>(end_iterator.group_pointer->last_endpoint - end_iterator.group_pointer->elements);
+		
+		return data;
 	}
 
 

SG14_test/flat_map_test.cpp
@@ -30,9 +30,9 @@ struct InstrumentedWidget {
     static int move_ctors, copy_ctors;
     InstrumentedWidget() = delete;
     InstrumentedWidget(const char *s) : s_(s) {}
-    InstrumentedWidget(InstrumentedWidget&& o) : s_(std::move(o.s_)) { o.is_moved_from = true; move_ctors += 1; }
+    InstrumentedWidget(InstrumentedWidget&& o) noexcept : s_(std::move(o.s_)) { o.is_moved_from = true; move_ctors += 1; }
     InstrumentedWidget(const InstrumentedWidget& o) : s_(o.s_) { copy_ctors += 1; }
-    InstrumentedWidget& operator=(InstrumentedWidget&& o) {
+    InstrumentedWidget& operator=(InstrumentedWidget&& o) noexcept {
         s_ = std::move(o.s_);
         o.is_moved_from = true;
         return *this;

SG14_test/inplace_function_test.cpp
@@ -18,14 +18,14 @@ static int expected;
 struct Functor {
     Functor() {}
     Functor(const Functor&) { copied += 1; }
-    Functor(Functor&&) { moved += 1; }
+    Functor(Functor&&) noexcept { moved += 1; }
     void operator()(int i) { assert(i == expected); called_with = i; }
 };
 
 struct ConstFunctor {
     ConstFunctor() {}
     ConstFunctor(const ConstFunctor&) { copied += 1; }
-    ConstFunctor(ConstFunctor&&) { moved += 1; }
+    ConstFunctor(ConstFunctor&&) noexcept { moved += 1; }
     void operator()(int i) const { assert(i == expected); called_with = i; }
 };
 
@@ -103,7 +103,7 @@ struct AnotherFunctor
     int operator()(int x) { mTotal += x; return mTotal; }
 
     AnotherFunctor() { mConstructorCalls++; }
-    AnotherFunctor(AnotherFunctor&&) { mConstructorCalls++; }
+    AnotherFunctor(AnotherFunctor&&) noexcept { mConstructorCalls++; }
     AnotherFunctor(const AnotherFunctor&) { mConstructorCalls++; }
     ~AnotherFunctor() { mDestructorCalls++; }
 };
@@ -523,7 +523,7 @@ struct InstrumentedCopyConstructor {
     InstrumentedCopyConstructor(const InstrumentedCopyConstructor&) {
         copies += 1;
     }
-    InstrumentedCopyConstructor(InstrumentedCopyConstructor&&) {
+    InstrumentedCopyConstructor(InstrumentedCopyConstructor&&) noexcept {
         moves += 1;
     }
 };

SG14_test/plf_colony_test.cpp
@@ -135,21 +135,21 @@
 namespace
 {
 
-    inline void failpass(const char *test_type, bool condition)
-    {
-        if (!condition)
-        {
-				printf("%s: Fail\n", test_type);
-				getchar();
-            abort();
-        }
-    }
+	inline void failpass(const char* test_type, bool condition)
+	{
+		if (!condition)
+		{
+			printf("%s: Fail\n", test_type);
+			getchar();
+			abort();
+		}
+	}
 
-	void title1(const char *)
+	void title1(const char*)
 	{
 	}
 
-	void title2(const char *)
+	void title2(const char*)
 	{
 	}
 
@@ -192,12 +192,13 @@ namespace
 	};
 
 
+
 	struct small_struct
 	{
-		double *empty_field_1;
+		double* empty_field_1;
 		double unused_number;
 		unsigned int empty_field2;
-		double *empty_field_3;
+		double* empty_field_3;
 		int number;
 		unsigned int empty_field4;
 
@@ -205,17 +206,36 @@ namespace
 	};
 
 
+
 	class non_copyable_type
 	{
 	private:
 		int i;
-		non_copyable_type(const non_copyable_type &); // non construction-copyable
-		non_copyable_type& operator=(const non_copyable_type &); // non copyable
+		non_copyable_type(const non_copyable_type&); // non construction-copyable
+		non_copyable_type& operator=(const non_copyable_type&); // non copyable
 	public:
 		non_copyable_type(int a) : i(a) {}
 	};
 
 #endif
+
+
+
+
+	int global_counter = 0;
+
+	struct small_struct_non_trivial
+	{
+		double* empty_field_1;
+		double unused_number;
+		unsigned int empty_field2;
+		double* empty_field_3;
+		int number;
+		unsigned int empty_field4;
+
+		small_struct_non_trivial(const int num) PLF_NOEXCEPT: number(num) {};
+		~small_struct_non_trivial() { ++global_counter; };
+	};
 }
 
 
@@ -513,12 +533,9 @@ void plf_colony_test()
 			i_colony.clear();
 			i_colony.change_minimum_group_size(10000);
 			
-			for (unsigned int temp = 0; temp != 30000; ++temp)
-			{
-				i_colony.insert(1);
-			}
+			i_colony.insert(30000, 1); // fill-insert 30000 elements
 			
-			failpass("Size after reinitialize + insert test", i_colony.size() == 30000);
+			failpass("Size after reinitialize + fill-insert test", i_colony.size() == 30000);
 
 			unsigned short count2 = 0;
 			
@@ -541,10 +558,7 @@ void plf_colony_test()
 			
 			failpass("Erase randomly till half-empty test", i_colony.size() == 30000u - count2);
 
-			for (unsigned int temp = 0; temp != count2; ++temp)
-			{
-				i_colony.insert(1);
-			}
+			i_colony.insert(count2, 1);
 			
 			failpass("Size after reinsert test", i_colony.size() == 30000);
 
@@ -589,10 +603,7 @@ void plf_colony_test()
 			failpass("Random insert/erase till empty test", i_colony.size() == 0);
 
 			
-			for (unsigned int temp = 0; temp != 500000; ++temp)
-			{
-				i_colony.insert(10);
-			}
+			i_colony.insert(500000, 10);
 
 			failpass("Insert post-erase test", i_colony.size() == 500000);
 			colony<int>::iterator it2 = i_colony.begin();
@@ -606,11 +617,7 @@ void plf_colony_test()
 			
 			failpass("Large multi-increment iterator test", i_colony.size() == 250000);
 
-			
-			for (unsigned int temp = 0; temp != 250000; ++temp)
-			{
-				i_colony.insert(10);
-			}
+			i_colony.insert(250000, 10);
 			
 			colony<int>::iterator end_iterator = i_colony.end();
 			i_colony.advance(end_iterator, -250000);
@@ -622,13 +629,8 @@ void plf_colony_test()
 
 			failpass("Large multi-decrement iterator test", i_colony.size() == 250000);
 
-			
-			for (int temp = 0; temp != 250000; ++temp)
-			{
-				i_colony.insert(10);
-			}
-			
-			
+
+			i_colony.insert(250000, 10);
 			int total = 0;
 			
 			for (colony<int>::iterator the_iterator = i_colony.begin(); the_iterator != i_colony.end(); ++the_iterator)
@@ -652,10 +654,7 @@ void plf_colony_test()
 			failpass("Non-end decrement + erase test", i_colony.size() == 350001);
 			
 
-			for (unsigned int temp = 0; temp != 100000; ++temp)
-			{
-				i_colony.insert(10);
-			}
+			i_colony.insert(100000, 10);
 			
 			begin_iterator = i_colony.begin();
 			i_colony.advance(begin_iterator, 300001);
@@ -978,7 +977,7 @@ void plf_colony_test()
 				i_colony.clear();
 				internal_loop_counter = 0;
 				
-				i_colony.insert(10000, 10); // fill-insert
+				i_colony.insert(10000, 10);
 				
 				while (!i_colony.empty())
 				{
@@ -1062,6 +1061,150 @@ void plf_colony_test()
 			i_colony.erase(i_colony.begin(), i_colony.begin());
 
 			failpass("Range-erase when range is empty test (crash test)", i_colony.size() == 10);
+
+
+
+			i_colony.insert(10000, 5);
+
+			int sum1 = 0, sum2 = 0;
+			range1 = 0;
+			range2 = 0;
+
+
+			// Erase half of all elements and sum the rest:
+			for (colony<int>::iterator it = i_colony.begin(); it != i_colony.end(); ++it)
+			{
+				it = i_colony.erase(it);
+				sum1 += *it;
+				++range1;
+			}
+
+			
+			colony<int>::raw_memory_block_pointers *data = i_colony.get_raw_memory_block_pointers();
+
+			// Manually sum using raw memory blocks:
+			for (unsigned int block_num = 0; block_num != data->number_of_blocks; ++block_num)
+			{
+				for (unsigned short block_sub_index = 0; block_sub_index != data->block_sizes[block_num]; ++block_sub_index)
+				{
+					if ((data->skipfield_memory_block_pointers[block_num])[block_sub_index] == 0)
+					{
+						// We have to reinterpret_cast (via pointers) back to the original type since colony stores data internally as an aligned version of the same type
+						sum2 += *reinterpret_cast<int *>((data->element_memory_block_pointers[block_num]) + block_sub_index);
+						++range2;
+					}
+				}
+			}
+
+			delete data;
+
+			failpass("Manual summing pass over elements gotten from get_raw_memory_block_pointers()", (sum1 == sum2) && (range1 == range2));
+		}
+
+
+
+		{
+			title1("Non-trivial type tests");
+			
+			colony<small_struct_non_trivial> ss_nt;
+			colony<small_struct_non_trivial>::iterator ss_it1, ss_it2;
+			
+			small_struct_non_trivial ss(5);
+			
+			unsigned int size, range1 = 0, range2 = 0, internal_loop_counter;
+			int counter, sum1 = 0, sum2 = 0;
+
+			ss_nt.insert(10000, ss);
+
+			failpass("Non-trivial type insert test", ss_nt.size() == 10000);
+
+
+			for (colony<small_struct_non_trivial>::iterator ss_it = ss_nt.begin(); ss_it != ss_nt.end(); ++ss_it)
+			{
+				ss_it = ss_nt.erase(ss_it);
+				sum1 += ss_it->number;
+				++range1;
+			}
+
+			failpass("Non-trivial type erase half of all elements", ss_nt.size() == 5000);
+			
+			
+			colony<small_struct_non_trivial>::raw_memory_block_pointers *data = ss_nt.get_raw_memory_block_pointers();
+
+			// Manually pass over contents:
+			for (unsigned int block_num = 0; block_num != data->number_of_blocks; ++block_num)
+			{
+				for (unsigned short block_sub_index = 0; block_sub_index != data->block_sizes[block_num]; ++block_sub_index)
+				{
+					if (*(data->skipfield_memory_block_pointers[block_num] + block_sub_index) == 0)
+					{
+						// We have to reinterpret_cast (via pointers) back to the original type since the colony stores data as an aligned version of the same type
+						sum2 += (reinterpret_cast<small_struct_non_trivial *>(data->element_memory_block_pointers[block_num] + block_sub_index))->number;
+						++range2;
+					}
+				}
+			}
+
+			delete data;
+
+			failpass("Non-trivial manual summing pass over elements gotten from get_raw_memory_block_pointers()", (sum1 == sum2) && (range1 == range2));
+
+			
+			for (unsigned int loop_counter = 0; loop_counter != 50; ++loop_counter)
+			{
+				ss_nt.clear();
+
+				for (counter = 0; counter != 1000; ++counter)
+				{
+					ss_nt.insert(counter);
+				}
+				
+				internal_loop_counter = 0;
+
+				while (!ss_nt.empty())
+				{
+					ss_it2 = ss_it1 = ss_nt.begin();
+
+					size = static_cast<unsigned int>(ss_nt.size());
+					range1 = xor_rand() % size;
+					range2 = range1 + 1 + (xor_rand() % (size - range1));
+					ss_nt.advance(ss_it1, range1);
+					ss_nt.advance(ss_it2, range2);
+
+					ss_nt.erase(ss_it1, ss_it2);
+
+					counter = 0;
+
+					for (colony<small_struct_non_trivial>::iterator ss_it = ss_nt.begin(); ss_it != ss_nt.end(); ++ss_it)
+					{
+						++counter;
+					}
+
+					if (ss_nt.size() != static_cast<unsigned int>(counter))
+					{
+						printf("Fuzz-test range-erase randomly Fail: loop counter: %u, internal_loop_counter: %u.\n", loop_counter, internal_loop_counter);
+						getchar(); 
+						abort(); 
+					}
+					
+					if (ss_nt.size() != ss_nt.group_size_sum())
+					{
+						printf("Fuzz-test range-erase randomly Fail - group_size_sum failure: loop counter: %u, internal_loop_counter: %u, size: %u, group_size_sum: %u.\n", loop_counter, internal_loop_counter, static_cast<unsigned int>(ss_nt.size()), static_cast<unsigned int>(ss_nt.group_size_sum()));
+						getchar(); 
+						abort(); 
+					}
+					
+					if (ss_nt.size() > 2)
+					{ // Test to make sure our stored erased_locations are valid
+						ss_nt.insert(1);
+						ss_nt.insert(10);
+					}
+	
+					++internal_loop_counter;
+				}
+			}
+
+			failpass("Non-trivial type fuzz-test range-erase randomly until empty", ss_nt.size() == 0);
 		}
 
 
@@ -1247,6 +1390,8 @@ void plf_colony_test()
 
 			failpass("Non-copyable size test", temp.size() == 2);
 		}
+
+
 		#endif
 
 

SG14_test/uninitialized_test.cpp
@@ -19,7 +19,7 @@ namespace
 		{
 			++construct;
 		}
-		lifetest(lifetest&& /*in*/)
+		lifetest(lifetest&& /*in*/) noexcept
 		{
 			++move;
 		}

commit dfc29c0c0f8b0c901f90215d0a274b3747e67346

slot_map: Fix noexcept bugs introduced in 836e4d20.
And add regression tests so this shouldn't happen again.

SG14/slot_map.h
@@ -104,7 +104,7 @@ public:
 
     constexpr slot_map() = default;
     constexpr slot_map(const slot_map&) = default;
-    constexpr slot_map(slot_map&&) noexcept = default;
+    constexpr slot_map(slot_map&&) = default;
     constexpr slot_map& operator=(const slot_map&) = default;
     constexpr slot_map& operator=(slot_map&&) = default;
     ~slot_map() = default;

SG14_test/slot_map_test.cpp
@@ -51,14 +51,14 @@ struct Vector {
     Vector() = default;
     template<class T_ = T, class = std::enable_if_t<std::is_copy_constructible<T_>::value>>
     Vector(const Vector& rhs) { *this = rhs; }
-    Vector(Vector&& rhs) noexcept { *this = std::move(rhs); }
+    Vector(Vector&& rhs) { *this = std::move(rhs); }
     template<class T_ = T, class = std::enable_if_t<std::is_copy_constructible<T_>::value>>
     void operator=(const Vector& rhs) {
         size_ = rhs.size_;
         data_ = std::make_unique<T[]>(size_);
         std::copy(rhs.begin(), rhs.end(), data_.get());
     }
-    void operator=(Vector&& rhs) noexcept {
+    void operator=(Vector&& rhs) {
         size_ = rhs.size_;
         data_ = std::move(rhs.data_);
     }
@@ -86,9 +86,8 @@ struct Vector {
         data_ = nullptr;
     }
     friend void swap(Vector& a, Vector& b) {
-        Vector t = std::move(a);
-        a = std::move(b);
-        b = std::move(t);
+        std::swap(a.size_, b.size_);
+        std::swap(a.data_, b.data_);
     }
 private:
     size_t size_ = 0;
@@ -515,6 +514,7 @@ void sg14_test::slot_map_test()
 
     // Test the most basic slot_map.
     using slot_map_1 = stdext::slot_map<int>;
+    static_assert(std::is_nothrow_move_constructible<slot_map_1>::value, "preserve nothrow-movability of vector");
     BasicTests<slot_map_1>(42, 37);
     BoundsCheckingTest<slot_map_1>();
     FullContainerStressTest<slot_map_1>([]() { return 1; });
@@ -569,6 +569,7 @@ void sg14_test::slot_map_test()
 
     // Test slot_map with a custom (non-standard, random-access) container type.
     using slot_map_5 = stdext::slot_map<int, std::pair<unsigned, unsigned>, TestContainer::Vector>;
+    static_assert(!std::is_nothrow_move_constructible<slot_map_5>::value, "preserve non-nothrow-movability of Vector");
     BasicTests<slot_map_5>(415, 315);
     BoundsCheckingTest<slot_map_5>();
     FullContainerStressTest<slot_map_5>([]() { return 37; });
@@ -582,6 +583,8 @@ void sg14_test::slot_map_test()
 
     // Test slot_map with a custom (standard, bidirectional-access) container type.
     using slot_map_6 = stdext::slot_map<int, std::pair<unsigned, unsigned>, std::list>;
+    static_assert(std::is_nothrow_move_constructible<slot_map_6>::value == std::is_nothrow_move_constructible<std::list<int>>::value,
+                  "preserve implementation-defined nothrow-movability of std::list");
     BasicTests<slot_map_6>(415, 315);
     BoundsCheckingTest<slot_map_6>();
     FullContainerStressTest<slot_map_6>([]() { return 37; });